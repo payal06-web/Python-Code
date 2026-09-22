41d4245b7ec63bb995334a59d185a8ed6851d820
payal06-web <pihuuprajapat4444@gmail.com>
2026-09-22 13:12:04 +0530

    Add files via upload

---
+++ day2-Variables.py
@@ -0,0 +1,15 @@
+# Comments
+#I am learning python
+
+#Variables
+a = 1
+
+#Naming Conventions
+# 1. camel case
+firstVariable = 0
+
+# 2. pascal case
+SecondVariable = 1
+
+# 3. Snake case
+third_variable = 2
