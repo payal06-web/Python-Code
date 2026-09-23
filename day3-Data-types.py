298b38836c99d4dd33d6e3b9487f24cec401fe75
payal06-web <pihuuprajapat4444@gmail.com>
2026-09-23 10:18:09 +0530

    Add files via upload

---
+++ day3-Data-types.py
@@ -0,0 +1,18 @@
+# we have three types in data types
+
+# 1.Number - Int, Float , Boolean
+#            a = 1
+#            print(type(a))    int
+             
+#            a = 1.4
+#            print(type(a))    float
+
+#            a = 22f
+#            print(type(a))    complex
+# 
+# 2. String
+#           a = "world"
+# 
+# 3. Boolean
+#           a = True
+#           b = False
