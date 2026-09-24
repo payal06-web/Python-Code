a3b2e91d4e12e641e511d7b65f8c50e2cd06cf5c
payal06-web <pihuuprajapat4444@gmail.com>
2026-09-24 10:22:45 +0530

    Add files via upload

---
+++ day4-string&Typeconversion.py
@@ -0,0 +1,22 @@
+# String
+#   string indexing:
+#      a = "Nature"
+#      print(a[0])  output:N 
+
+#   string slicing:
+#      a = "Sheriyans Coding"
+#      a[start, stop, steps]
+#      print(a[0:9:1])  output: Sheriyans
+
+
+
+
+# Type conversions
+# 1. a = 2
+#    a = str(a)
+#    print(type(a))
+
+# Take input from user in python 
+# age = int(input("Enter age"))
+# print(f"My age is {age}")
+# type of age is int not a string
